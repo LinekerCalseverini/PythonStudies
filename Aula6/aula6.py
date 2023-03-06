ebc9cd8c6b791b32642c23252e05b60c778bdbdd
Lineker Calseverini <lineker.calseverini@gmail.com>
2023-03-06 17:03:55 -0300

    Aula6 terminada

---
+++ Aula6/aula6.py
@@ -0,0 +1,20 @@
+# conversão de tipos, coerção
+# type conversion, typecasting, coercion
+# é o ato de converter um tipo em outro
+# tipos imutáveis e primitivos:
+# str, int, float, bool
+
+# Casting para inteiro a partir de uma string
+print(int('1'), type(int(1)))
+
+# Casting para float a partir de uma string
+print(type(float('1') + 1))
+
+# String com valor = True
+print(bool(' '))
+
+# String vazia = False
+print(bool(' '))
+
+# Casting para string a partir de um inteiro
+print(str(11) + 'b')
