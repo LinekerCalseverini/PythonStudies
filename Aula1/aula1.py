3266a81b6a10d8d4ccaa98203eace742e1596b62
Lineker Calseverini <lineker.calseverini@gmail.com>
2023-03-06 13:30:36 -0300

    Aula1 realmente terminada

--- Aula1/aula1.py
+++ Aula1/aula1.py
@@ -1,4 +1,8 @@
 #!/usr/local/bin/python3
-print('Atenção!')
-print('Opa, editei.')
-print('Opa, editei.')
+# \r\n -> CRLF
+# \n   -> L
+# sep=parâmetro de separação da função print
+# end=parâmetro de encerramento da função print
+print(12, 34, sep="-", end='\n')
+print(56, 78, sep='-', end ='\n')
+print(9, 10, sep='-', end='\n')
--- Dockerfiles/aula1.dockerfile
+++ Dockerfiles/aula1.dockerfile
@@ -2,6 +2,6 @@ FROM python:3.11.2-alpine3.17
 
 WORKDIR /app
 
-COPY . .
+COPY ./Aula1 .
 
 CMD ["python3","aula1.py"]
