d4df9ea6802a05897b319013338860a798d18c40
Lineker Calseverini <lineker.calseverini@gmail.com>
2023-03-07 11:54:49 -0300

    primeiro commit aula 7

---
+++ Aula8/aula8.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python3
+nome = 'Lineker'
+sobrenome = 'Calseverini'
+idade = 27
+ano_nascimento = 1996
+maior_de_idade = idade >= 27
+altura_metros = 1.79
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
