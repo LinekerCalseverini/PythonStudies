de08a9a7b4c226a8e6a3f24f0f4db03ef23666da
Lineker Calseverini <lineker.calseverini@gmail.com>
2023-03-07 16:44:14 -0300

    Aula 9 finalizada

--- Aula9/aula9.py
+++ Aula9/aula9.py
@@ -8,7 +8,7 @@ print('Subtração', subtracao)
 multiplicacao = 10 * 10
 print('Multiplicação', multiplicacao)
 
-divisao = 10 / 2.2
+divisao = 10 / 2.2 # float
 print('Divisão', divisao)
 
 divisao_inteira = 10 // 2.2
@@ -17,3 +17,11 @@ print('Divisão inteira', divisao_inteira)
 exponenciacao = 2 ** 10
 print('Exponenciação', exponenciacao)
 
+modulo = 55 % 2 # resto da divisão
+print('Módulo', modulo)
+
+print(10 % 8 == 0)
+print(16 % 8 == 0)
+print(10 % 2 == 0)
+print(15 % 2 == 0)
+print(16 % 2 == 0)
