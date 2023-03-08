4f660ea564a84b5e34ddd0698ab48aef12412982
Lineker Calseverini <lineker.calseverini@gmail.com>
2023-03-08 00:19:39 -0300

    Commit inicial Aula 9

---
+++ Aula12/aula12.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python3
+# Exercício (clássico): cálculo de IMC
+nome = 'Luiz Otávio'
+altura = 1.80
+peso = 95
+imc = peso / altura ** 2
+
+# Luiz Otávio tem 1.80 de altura,
+# pesa 95 quilos e seu IMC é
+# 29.320987654320987
+
+print(f'{nome} tem {altura} de altura, pesa {peso} e seu IMC é {imc}')
