a3cc7c53359e6df9bbaa153c54cd4c1e2c8e1844
Lineker Calseverini <lineker.calseverini@gmail.com>
2023-03-09 12:35:28 -0300

    Aula 15 concluída

---
+++ Aula15/aula15.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python3
+nome = input('Qual o seu nome? ')
+print(f'O seu nome é {nome}')
+# Se quiser que apareça nome= na string você pode fazer da seguinte forma:
+# print(f'O seu nome é {nome=}')
+
+numero_1 = int(input('Digite um número: '))
+numero_2 = int(input('Digite outro número: '))
+
+print(f'A soma dos números é: {numero_1 + numero_2}')
