d5a04d907373293790980a594e356c6e5955e289
LinekerCalseverini <123635282+LinekerCalseverini@users.noreply.github.com>
2023-03-12 16:48:38 -0300

    Aula 19 concluída (#16)

---
+++ Aula19/aula19.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python3
+# Operadores de comparação (relacionais)
+# OP    Significado     Exemplo
+# >     maior           2 > 1
+# >=    maior/igual     2 >= 2
+# <     menor           1 < 2
+# <=    menor/igual     2 <= 2
+# ==    igual           'a'=='b'
+# !=    diferente       'a'!='b'
+
+maior = 2 > 1
+maior_ou_igual = 2 >= 2
+menor = 1 < 2
+menor_ou_igual = 2 <= 2
+igual = 'a' == 'a'
+diferente = 'a' != 'b'
+
+print(diferente)
---
+++ Dockerfiles/aula19.dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.11.2-alpine3.17
+
+WORKDIR /app
+
+COPY ./Aula19 .
+
+RUN chmod -R +x ./
+
+CMD ["python3", "aula19.py"]
