699c904b3e69188d79c0a1aa23c0716c91d5c0d5
Lineker Calseverini <lineker.calseverini@gmail.com>
2023-03-13 12:01:16 -0300

    Aula 20 - otimização do script para uso de menos memória

--- Aula20/aula20.py
+++ Aula20/aula20.py
@@ -1,12 +1,17 @@
 #!/usr/local/bin/python3
 # Exercício: Utilizando comparadores, escrever um programa que recebe dois valores e diz qual é o maior.
+# Neste exercício tentei reutilizar o máximo possível strings, simplesmente pra ver quanto de memória eu consigo economizar. Este método tornou o código imprático e difícil de ler, porém como é um código pequeno foi um bom método para praticar.
+
 primeiro_valor = input('Digite um valor: ')
 segundo_valor = input('Digite outro valor: ')
 
-resposta = ['primeiro',primeiro_valor,'segundo',segundo_valor]
+primeiro = 'primeiro'
+segundo = 'segundo'
+
+resposta = [primeiro,primeiro_valor,segundo,segundo_valor]
 
 if segundo_valor > primeiro_valor:
-    resposta = ['segundo',segundo_valor,'primeiro',primeiro_valor]
+    resposta = [segundo,segundo_valor,primeiro,primeiro_valor]
 
 if primeiro_valor != segundo_valor:
     resposta.append('maior que ')
