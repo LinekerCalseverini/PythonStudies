625e902b56eb3f69fbac86e533efdbbc554a27f1
Lineker Calseverini <lineker.calseverini@gmail.com>
2023-03-13 12:06:49 -0300

    Aula 20 realmente concluída

--- Aula20/aula20.py
+++ Aula20/aula20.py
@@ -2,20 +2,22 @@
 # Exercício: Utilizando comparadores, escrever um programa que recebe dois valores e diz qual é o maior.
 # Neste exercício tentei reutilizar o máximo possível strings, simplesmente pra ver quanto de memória eu consigo economizar. Este método tornou o código imprático e difícil de ler, porém como é um código pequeno foi um bom método para praticar.
 
-primeiro_valor = input('Digite um valor: ')
-segundo_valor = input('Digite outro valor: ')
+DIGITE = 'Digite'
+VALOR = 'valor'
+PRIMEIRO = 'primeiro'
+SEGUNDO = 'segundo'
 
-primeiro = 'primeiro'
-segundo = 'segundo'
+primeiro_valor = input(f'{DIGITE} um {VALOR}: ')
+segundo_valor = input(f'{DIGITE} outro {VALOR}: ')
 
-resposta = [primeiro,primeiro_valor,segundo,segundo_valor]
+resposta = [PRIMEIRO,primeiro_valor,SEGUNDO,segundo_valor]
 
 if segundo_valor > primeiro_valor:
-    resposta = [segundo,segundo_valor,primeiro,primeiro_valor]
+    resposta = [SEGUNDO,segundo_valor,PRIMEIRO,primeiro_valor]
 
 if primeiro_valor != segundo_valor:
     resposta.append('maior que ')
 else:
     resposta.append('igual a')
 
-print(f'O {resposta[0]} valor ({resposta[1]}) é {resposta[4]}o {resposta[2]} valor ({resposta[3]}).')
+print(f'O {resposta[0]} {VALOR} ({resposta[1]}) é {resposta[4]}o {resposta[2]} {VALOR} ({resposta[3]}).')
