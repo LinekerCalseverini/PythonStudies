374053540bf12026d8c0d0df7f7a5774f53c4933
Lineker Calseverini <lineker.calseverini@gmail.com>
2023-03-19 13:44:20 -0300

    Aula32 Iniciada - Exercícios

--- Aula30/aula30.py
+++ Aula30/aula30.py
@@ -1,23 +1,20 @@
-# Introdução ao try/except
-# try -> tentar executar o código
-# except -> ocorreu algum erro ao tentar executar
+# CONSTANTE = "Variáveis" que não vão mudar
+# Python não existe um conceito de variáveis constantes, mas a convenção é que constantes estejam todas em maiúsculo
 
-# print(1234)
-# print(456)
-# int('a')
+# Muitas condições no mesmo if (ruim) - evitar muitas condições de if
 
-numero_str = input('Vou dobrar o número que vc digitar: ')
+#    <- Contagem de complexidade (ruim) - Se houver muitos tabs seguidos de identação, é difícil de se ler o código
 
-try:
-    print('STR:', numero_str)
-    numero_float = float(numero_str)
-    print('FLOAT:', numero_float)
-    print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
-except ValueError:
-    print('Isso não é um número')
+velocidade = 61 # velocidade atual do carro
+local_carro = 101 # local em que o carro está na estrada
 
-# if numero_str.isdigit():
-#     numero_float = float(numero_str)
-#     print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
-# else:
-#     print('Isso não é um número')
+RADAR_1 = 60 # velocidade máxima do radar 1
+LOCAL_1 = 100 # local onde o radar 1 está
+RADAR_RANGE = 1 # A distância onde o radar pega
+
+if velocidade > RADAR_1:
+    print('Velocidade carro passou do radar 1')
+
+    if local_carro >= (LOCAL_1 - RADAR_RANGE) and \
+        local_carro <= (LOCAL_1 + RADAR_RANGE):
+        print('carro multado em radar 1')
