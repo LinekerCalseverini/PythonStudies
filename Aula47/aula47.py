4ea9ff11c01fc54bddd2f138f2ed750563c143e2
LinekerCalseverini <linekercalseverini@gmail.com>
2023-03-20 00:55:17 -0300

    Aula 47 com solução do professor

--- Aula47/aula47.py
+++ Aula47/aula47.py
@@ -13,11 +13,49 @@
 # Faça a contagem de tentativas do seu
 # usuário.
 
+# print('{:-^30}'.format('Exercício: Palavra Secreta'))
+# palavra_secreta = 'perfume'
+# palavra_revelada = '*******'
+# jogo_iniciado = True
+# tentativas = 0
+
+# while jogo_iniciado:
+#     chute = input('Digite uma letra: ')
+
+#     if len(chute) > 1:
+#         print('Digite apenas uma letra.')
+#         tentativas += 1
+#         continue
+
+#     if chute.isdigit():
+#         print('Digite um caracter alfabético.')
+#         tentativas += 1
+#         continue
+    
+#     if chute in palavra_secreta:
+#         i = 0
+#         while i < len(palavra_revelada):
+#             try:
+#                 if chute == palavra_secreta[i]:
+#                     palavra_revelada = f'{palavra_revelada[0:i]}{chute}'\
+#                                        f'{palavra_revelada[i+1:]}'
+#             except IndexError:
+#                 palavra_revelada = f'{palavra_revelada[0:i]}{chute}'
+#                 break
+#             i += 1
+    
+#     print(palavra_revelada)
+#     tentativas += 1
+#     jogo_iniciado = palavra_revelada != palavra_secreta
+
+# print(f'Parabéns, você levou {tentativas} tentativas para acertar.')
+
+# Solução do professor
 print('{:-^30}'.format('Exercício: Palavra Secreta'))
 palavra_secreta = 'perfume'
-palavra_revelada = '*******'
-jogo_iniciado = True
+letras_acertadas = ''
 tentativas = 0
+jogo_iniciado = True
 
 while jogo_iniciado:
     chute = input('Digite uma letra: ')
@@ -33,19 +71,18 @@ while jogo_iniciado:
         continue
     
     if chute in palavra_secreta:
-        i = 0
-        while i < len(palavra_revelada):
-            try:
-                if chute == palavra_secreta[i]:
-                    palavra_revelada = f'{palavra_revelada[0:i]}{chute}'\
-                                       f'{palavra_revelada[i+1:]}'
-            except IndexError:
-                palavra_revelada = f'{palavra_revelada[0:i]}{chute}'
-                break
-            i += 1
+        letras_acertadas += chute
     
-    print(palavra_revelada)
-    tentativas += 1
-    jogo_iniciado = palavra_revelada != palavra_secreta
+    palavra_formada = ''
+    for letra_secreta in palavra_secreta:
+        if letra_secreta in letras_acertadas:
+            palavra_formada += letra_secreta
+        else:
+            palavra_formada += '*'
 
-print(f'Parabéns, você levou {tentativas} tentativas para acertar.')
+    jogo_iniciado = palavra_formada != palavra_secreta
+
+    print(palavra_formada)
+    tentativas += 1
+else:
+    print(f'Parabéns, você levou {tentativas} tentativas para acertar.')
