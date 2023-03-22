106348ef0c574ea3e4e8a2a76116b0c16ec6d1d1
Lineker Calseverini <lineker.calseverini@gmail.com>
2023-03-22 13:05:54 -0300

    Aula 80 iniciada

--- Aula78/aula78.py
+++ Aula78/aula78.py
@@ -17,16 +17,34 @@
 # * - eles não tem indexes;
 # * - eles não garantem ordem;
 # * - eles são iteráveis (for, in, not in);
-l1 = [1, 2, 3, 3, 3, 3, 3, 1]
-s1 = set(l1)
-l2 = list(s1)
-print(l2)
+# l1 = [1, 2, 3, 3, 3, 3, 3, 1]
+# s1 = set(l1)
+# l2 = list(s1)
+s1 = {1, 2, 3}
+# print(s1)
+# for  numero in s1:
+#     print(numero)
 
 # * Métodos úteis:
 # * add, update, clear, discard
+s1 = set()
+s1.add('Luiz')
+s1.add(1)
+s1.update(('Olá mundo', 1, 2, 3, 4))
+# s1.clear()
+s1.discard('Olá mundo')
+s1.discard('Luiz')
+print(s1)
 
 # * Operadores úteis:
-# * união | união (union) - Une
-# * intersecção & (intersection) - Itens presentes em ambos
-# * diferença - Itens presentes apenas no set da esquerda
-# * diferença simétrica ^ - Itens que não estão em ambos
+# * união (|) união (union) - Une
+# * intersecção (&) (intersection) - Itens presentes em ambos
+# * diferença (-) - Itens presentes apenas no set da esquerda
+# * diferença simétrica (^) - Itens que não estão em ambos
+s1 = {1, 2, 3}
+s2 = {2, 3, 4}
+s3 = s1 | s2
+s3 = s1 & s2
+s3 = s1 - s2
+s3 = s1 ^ s2
+print(s3)
