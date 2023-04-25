154dca70687f0928838c0860f7ca8be8be6f6da0
Lineker Calseverini <lineker.calseverini@gmail.com>
2023-04-25 19:14:06 -0300

    Aula 156 iniciada

---
+++ Aula156/uma_linha.py
@@ -0,0 +1,6 @@
+"""O que seu módulo faz"""
+
+variavel = 'valor'
+
+def funcao():
+    return 1
---
+++ Aula156/varias_linhas.py
@@ -0,0 +1,48 @@
+"""O que seu módulo faz
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat commodo 
+suscipit. Phasellus nulla tortor, varius ut massa eu, ornare vestibulum diam. 
+Aenean tempus nisi nibh, in venenatis augue pellentesque a. Curabitur pharetra 
+consectetur urna. Donec id lorem sagittis, auctor arcu a, tempor libero. 
+Praesent luctus sodales tortor vel volutpat. Ut auctor nisl vitae ligula 
+tincidunt lobortis. Phasellus suscipit egestas magna, eget imperdiet est dapibus 
+non. Donec ultricies feugiat velit, vel auctor urna luctus eget.
+
+Fusce feugiat libero finibus sagittis vestibulum. Nulla consequat mi quis 
+dignissim accumsan. Suspendisse potenti. Nam sed nibh vitae eros commodo euismod 
+in nec dui. Pellentesque euismod orci quis nisi rhoncus, non fermentum augue 
+viverra. Nam lacus nunc, bibendum eu est at, maximus fringilla enim. Maecenas eu 
+accumsan nibh, ac varius massa.
+
+In hac habitasse platea dictumst. Sed luctus nisi leo, vel ornare purus ornare 
+eu. Nam in nulla ex. Ut eleifend, velit eu gravida consectetur, urna eros 
+consequat sem, vel fringilla dui ex eu ex. Etiam sit amet nisi non velit 
+consequat bibendum ut ac mi. Cras vehicula augue in dictum gravida. Sed eget sem 
+a neque luctus consectetur.
+
+Nam lacus magna, consequat in euismod non, viverra at leo. Vivamus ut sem 
+eleifend, varius justo et, rhoncus odio. Sed pellentesque cursus mauris in 
+rutrum. Donec sodales mollis accumsan. Sed eleifend quam id turpis ultrices 
+placerat. Mauris est nisl, facilisis nec tincidunt ut, vehicula vitae ipsum. In 
+gravida, eros vel laoreet mattis, risus erat lobortis metus, eu pretium lacus 
+turpis nec lacus. Quisque non quam facilisis, imperdiet lectus et, vestibulum 
+nibh. Sed massa enim, vehicula nec pulvinar nec, maximus nec nulla. Cras 
+imperdiet turpis sit amet pretium molestie. Curabitur sodales eros id justo 
+fringilla, scelerisque tristique dolor mollis. Cras rutrum lacus posuere iaculis 
+interdum. Suspendisse semper sapien massa, ut tempor ligula semper at. Vivamus 
+vulputate pellentesque lectus, sit amet posuere metus malesuada a. Etiam 
+rhoncus, quam non auctor lacinia, quam mi aliquet lectus, porta iaculis est 
+neque vel est. Cras sit amet metus felis.
+
+Donec mauris lectus, viverra et velit at, scelerisque maximus eros. Aenean 
+gravida consequat velit interdum ullamcorper. Curabitur in enim non nisl 
+interdum vehicula et vel ante. Morbi sit amet auctor justo. Aliquam congue ut 
+augue at dapibus. Duis posuere tincidunt ligula. Mauris pharetra est et rhoncus 
+consequat. Aliquam sagittis eros sit amet purus consequat, id elementum tellus 
+pharetra. Maecenas mattis ante eget hendrerit gravida. Donec dictum luctus 
+condimentum. Fusce faucibus, lacus at blandit consectetur, nibh erat tristique 
+nulla, a lacinia nisi purus at dui. Pellentesque eget purus pellentesque ex 
+semper placerat vel ullamcorper est. Praesent porta est id cursus dictum. 
+Praesent quis elit urna. Nunc pharetra quam vitae ullamcorper ultricies. Donec 
+libero ante, imperdiet vitae pellentesque sed, laoreet sit amet nisi.
+"""
