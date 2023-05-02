2c9f6d8a07594b176c2171e071bab165422929a4
LinekerCalseverini <lineker.calseverini@gmail.com>
2023-05-02 08:06:16 -0300

    Aula 162 concluída

--- Aula162/aula162.py
+++ Aula162/aula162.py
@@ -11,6 +11,22 @@
 # Instalando o pytz
 # pip install pytz types-pytz
 from datetime import datetime
+from pytz import timezone
 
-data = datetime(2022, 4, 20)
-print(data)
+data = datetime.now(timezone('Asia/Tokyo'))
+print(data.timestamp())  # Isso está na base de dados
+print(datetime.fromtimestamp(1683024930))
+
+# * Meus Testes
+# * data_str = datetime.fromtimestamp(1683024930)
+# * data = datetime.strftime(data_str, '%d/%m/%Y %H:%M:%S')
+# * print(data)
+
+
+# data = datetime(2022, 4, 20, 7, 49, 23, tzinfo=timezone('Asia/Tokyo'))
+# print(data)
+
+# data_str = '2022/04/20 07:49:23'
+# data_str = '20/04/2022 07:49:23'
+# data_fmt = '%d/%m/%Y %H:%M:%S'
+# data = datetime.strptime(data_str, data_fmt)
