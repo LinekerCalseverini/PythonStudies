74bf968d5492f680c54b8e2c36a38966b249e99e
LinekerCalseverini <lineker.calseverini@gmail.com>
2023-05-06 13:23:38 -0300

    Aula 180 iniciada

---
+++ Aula180/aula180.py
@@ -0,0 +1,33 @@
+# csv.reader e csv.DictReader
+# csv.reader lê o CSV em formato de lista
+# csv. DictReader lê o CSV em formato de dicionário
+import csv
+from pathlib import Path
+
+CAMINHO_CSV = Path(__file__).parent / 'aula180.csv'
+
+lista_clientes = [
+    {'Nome': 'Luiz Otávio', 'Endereço': 'Av 1, 22'},
+    {'Nome': 'João Silva', 'Endereço': 'R. 2, "1"'},
+    {'Nome': 'Maria Sol', 'Endereço': 'Av B, 3A'}
+]
+with open(CAMINHO_CSV, 'w') as arquivo:
+    nome_colunas = lista_clientes[0].keys()
+    escritor = csv.writer(arquivo)
+
+# lista_clientes = [
+#     ['Luiz Otávio', 'Av 1, 22'],
+#     ['João Silva', 'R. 2, "1"'],
+#     ['Maria Sol', 'Av B, 3A'],
+# ]
+# with open(CAMINHO_CSV, 'w') as arquivo:
+#     # nome_colunas = lista_clientes[0].keys()
+#     nome_colunas = ['Nome', 'Endereço']
+#     escritor = csv.writer(arquivo)
+
+#     escritor.writerow(nome_colunas)
+
+#     for cliente in lista_clientes:
+#         escritor.writerow(cliente)
+
+# print(lista_clientes[0].keys())
