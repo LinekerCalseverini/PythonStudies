f693abd4937b3bc0f802dabc8a80610803bcdd22
LinekerCalseverini <lineker.calseverini@gmail.com>
2023-08-23 00:45:57 -0300

    Criando displays

--- Aula202/display.py
+++ Aula202/display.py
@@ -1,5 +1,6 @@
+from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QLineEdit
-from variables import BIG_FONT_SIZE
+from variables import BIG_FONT_SIZE, TEXT_MARGIN, MINIMUM_WIDTH
 
 
 class Display(QLineEdit):
@@ -10,4 +11,6 @@ class Display(QLineEdit):
     def configStyle(self):
         self.setStyleSheet(f'font-size: {BIG_FONT_SIZE}px;')
         self.setMinimumHeight(BIG_FONT_SIZE * 2)
-        ...
+        self.setMinimumWidth(MINIMUM_WIDTH)
+        self.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.setTextMargins(*[TEXT_MARGIN for _ in range(4)])
