6072e41576f53849be2e930c752f797b4c68a2d1
LinekerCalseverini <lineker.calseverini@gmail.com>
2023-08-23 22:06:35 -0300

    Criacao de label de informacao na calc

--- Aula202/main.py
+++ Aula202/main.py
@@ -1,7 +1,8 @@
 import sys
 
-from main_window import MainWindow
 from display import Display
+from info import Info
+from main_window import MainWindow
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import (QApplication)
 from variables import WINDOW_ICON_PATH
@@ -14,6 +15,10 @@ if __name__ == '__main__':
     icon = QIcon(str(WINDOW_ICON_PATH))
     window.setWindowIcon(icon)
     app.setWindowIcon(icon)
+
+    # Info
+    info = Info('2.0 ^ 10.0 = 1024.0')
+    window.addToVLayout(info)
 
     # Display
     display = Display()
