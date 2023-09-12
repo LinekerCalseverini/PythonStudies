6919ad8fa8acbf5e4bdd905ebce76eaea91f0f0c
LinekerCalseverini <lineker.calseverini@gmail.com>
2023-09-12 19:06:32 -0300

    [Aula 206] Trabalhando com MySQL

---
+++ Aula206/main.py
@@ -0,0 +1,49 @@
+# PyMySQL - um cliente MySQL feito em Python Puro
+# Doc: https://pymysql.readthedocs.io/en/latest/
+# Pypy: https://pypi.org/project/pymysql/
+# GitHub: https://github.com/PyMySQL/PyMySQL
+import os
+
+import pymysql
+import dotenv
+
+TABLE_NAME = 'customers'
+
+dotenv.load_dotenv()
+
+connection = pymysql.connect(
+    host=os.environ['MYSQL_HOST'],
+    user=os.environ['MYSQL_USER'],
+    passwd=os.environ['MYSQL_PASSWORD'],
+    database=os.environ['MYSQL_DATABASE']
+)
+
+with connection:
+    with connection.cursor() as cursor:
+        cursor.execute(
+            f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} ('
+            'id INT NOT NULL AUTO_INCREMENT, '
+            'nome VARCHAR(50) NOT NULL, '
+            'idade INT NOT NULL, '
+            'PRIMARY KEY (id)'
+            ')'
+        )
+        # CUIDADO: ISSO LIMPA A TABELA
+        cursor.execute(f'TRUNCATE TABLE {TABLE_NAME}')
+    connection.commit()
+
+    # Começo a manipular dados a partir daqui
+
+    with connection.cursor() as cursor:
+        sql = (
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) '
+            'VALUES '
+            '(%s, %s) '
+        )
+        data = ('Luiz', 18)
+        result = cursor.execute(sql, ('Luiz', 18))
+        print(sql, data)
+        print(result)
+
+    connection.commit()
