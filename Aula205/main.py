0fa2ade5c5a221a7d0d3356b7bd62d3a5e7ece12
LinekerCalseverini <lineker.calseverini@gmail.com>
2023-09-12 00:30:37 -0300

    [Aula 205] CRUD no SQLite3

--- Aula205/main.py
+++ Aula205/main.py
@@ -9,10 +9,15 @@ TABLE_NAME = 'customers'
 connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
+# CRUD - Create Read   Update Delete
+# SQL  - INSERT SELECT UPDATE DELETE
+
 # CUIDADO: fazendo delete sem where
 cursor.execute(
     f'DELETE FROM {TABLE_NAME}'
 )
+
+# DELETE mais cuidadoso
 cursor.execute(
     f'DELETE FROM sqlite_sequence WHERE name="{TABLE_NAME}"'
 )
@@ -31,13 +36,53 @@ connection.commit()
 
 # Registrar valores nas colunas da tabela
 # CUIDADO: sql injection
-cursor.execute(
+sql = (
     f'INSERT INTO {TABLE_NAME} '
-    '(id, name, weight) '
+    '(name, weight) '
     'VALUES '
-    '(NULL, "Helena", 4), (NULL, "Eduardo", 10)'
+    '(:nome, :weight)'
 )
+# cursor.executemany(sql, (('Joana', 4), ('Luiz', 5)))
+cursor.execute(sql, {'nome': 'Sem nome', 'weight': 3})
+cursor.executemany(sql, [
+    {'nome': 'Joãozinho', 'weight': 3},
+    {'nome': 'Maria', 'weight': 2},
+    {'nome': 'Helena', 'weight': 4},
+    {'nome': 'Joana', 'weight': 5},
+])
 connection.commit()
 
-cursor.close()
-connection.close()
+
+if __name__ == '__main__':
+    print(sql)
+
+    cursor.execute(
+        f'DELETE FROM {TABLE_NAME} '
+        'WHERE id = 3'
+    )
+    connection.commit()
+
+    cursor.execute(
+        f'DELETE FROM {TABLE_NAME} '
+        'WHERE id = 1'
+    )
+    connection.commit()
+
+    cursor.execute(
+        f'UPDATE {TABLE_NAME} '
+        'SET name = "QUALQUER", weight = 67.89 '
+        'WHERE id = 2'
+    )
+    connection.commit()
+
+    cursor.execute(
+        f'SELECT * FROM {TABLE_NAME} '
+    )
+    connection.commit()
+
+    for row in cursor.fetchall():
+        _id, name, weight = row
+        print(_id, name, weight)
+
+    cursor.close()
+    connection.close()
