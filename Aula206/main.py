0fa923966ed0052e8499767090f68936234c8610
LinekerCalseverini <lineker.calseverini@gmail.com>
2023-09-13 17:59:49 -0300

    [Aula 206] Tipos de Cursores

--- Aula206/main.py
+++ Aula206/main.py
@@ -6,6 +6,7 @@ import os
 
 import pymysql
 import dotenv
+from pymysql.cursors import SSDictCursor
 
 TABLE_NAME = 'customers'
 
@@ -15,7 +16,8 @@ connection = pymysql.connect(
     host=os.environ['MYSQL_HOST'],
     user=os.environ['MYSQL_USER'],
     passwd=os.environ['MYSQL_PASSWORD'],
-    database=os.environ['MYSQL_DATABASE']
+    database=os.environ['MYSQL_DATABASE'],
+    cursorclass=SSDictCursor  # type: ignore
 )
 
 with connection:
@@ -42,8 +44,117 @@ with connection:
             '(%s, %s) '
         )
         data = ('Luiz', 18)
-        result = cursor.execute(sql, ('Luiz', 18))
-        print(sql, data)
-        print(result)
+        result = cursor.execute(sql, data)
+        # print(sql, data)
+        # print(result)
 
     connection.commit()
+
+    with connection.cursor() as cursor:
+        sql = (
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) '
+            'VALUES '
+            '(%(name)s, %(age)s) '
+        )
+        data2 = {'name': 'Le', 'age': 27}
+
+        result = cursor.execute(sql, data2)
+        # print(sql)
+        # print(data2)
+        # print(result)
+
+    connection.commit()
+
+    with connection.cursor() as cursor:
+        sql = (
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) '
+            'VALUES '
+            '(%(name)s, %(age)s) '
+        )
+        data3 = (
+            {'name': 'Sah', 'age': 33},
+            {'name': 'Julia', 'age': 74},
+            {'name': 'Rose', 'age': 53},
+        )
+        result = cursor.executemany(sql, data3)  # type: ignore
+        # print(sql)
+        # print(data3)
+        # print(result)
+
+    connection.commit()
+
+    with connection.cursor() as cursor:
+        sql = (
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) '
+            'VALUES '
+            '(%s, %s) '
+        )
+        data4 = (
+            ('Siri', 22),
+            ('Helena', 15),
+            ('Luiz', 8)
+        )
+        result = cursor.executemany(sql, data4)  # type: ignore
+        # print(sql)
+        # print(data4)
+        # print(result)
+
+    connection.commit()
+
+    # Lendo os valores com SELECT
+    with connection.cursor() as cursor:
+        # menor_id = input('Digite um id: ')
+        # maior_id = input('Digite um id: ')
+        menor_id = 2
+        maior_id = 4
+
+        sql = (
+            f'SELECT * FROM {TABLE_NAME} '
+            'WHERE id BETWEEN %s AND %s'
+        )
+        cursor.execute(sql, (menor_id, maior_id))  # type: ignore
+        # print(cursor.mogrify(sql, (menor_id, maior_id)))  # type: ignore
+        data5 = cursor.fetchall()
+
+        # for row in data5:
+        #     print(row)
+
+    # Apagando com DELETE
+    with connection.cursor() as cursor:
+        sql = (
+            f'DELETE FROM {TABLE_NAME} '
+            'WHERE id = %s'
+        )
+        cursor.execute(sql, (1,))
+        connection.commit()
+
+        cursor.execute(f'SELECT * FROM {TABLE_NAME}')
+
+        # for row in cursor.fetchall():
+        #     print(row)
+
+    # Editando com UPDATE
+    with connection.cursor() as cursor:
+        sql = (
+            f'UPDATE {TABLE_NAME} '
+            'SET nome=%s, idade=%s '
+            'WHERE id=%s'
+        )
+        cursor.execute(sql, ('Eleonor', 102, 4))
+        connection.commit()
+
+        cursor.execute(f'SELECT * FROM {TABLE_NAME}')
+
+        print('For 1:')
+        for row in cursor.fetchall_unbuffered():
+            print(row)
+
+            if row['id'] >= 5:
+                break
+
+        print('\nFor 2:')
+        for row in cursor.fetchall_unbuffered():
+            print(row)
